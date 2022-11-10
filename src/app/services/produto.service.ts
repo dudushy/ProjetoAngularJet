1eaecd02006db4aead9cbc482a47149acbb7a707
dudushy <eduardotalarico1337@gmail.com>
2022-11-09 23:03:17 -0300

    Update produto.service.ts

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -6,24 +6,31 @@ import { Produto } from '../models/Produto';
   providedIn: 'root'
 })
 export class ProdutoService {
-  produtos : Produto [] = [
-    {idProduto:1 , nome:"Vinisco", preco:500 },
-    {idProduto:2 , nome:"Marcello", preco:500 },
-    {idProduto:3 , nome:"Pandora", preco:99999999999 },
+  title = 'ProdutoService';
 
-
+  produtos: Produto[] = [
+    { idProduto: 1, nome: "Vinisco", preco: 500 },
+    { idProduto: 2, nome: "Marcello", preco: 500 },
+    { idProduto: 3, nome: "Pandora", preco: 99999999999 }
   ]
-  constructor(private httpClient: HttpClient) { }
 
-  addProdutos(produto: Produto){
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    console.log(`[${this.title}#constructor]`);
+  }
+
+  addProdutos(produto: Produto) {
+    console.log(`[${this.title}#addProdutos] produto:`, produto);
     this.produtos.push(produto);
   }
 
-  getProdutos(){
+  getProdutos() {
+    console.log(`[${this.title}#getProdutos]`);
     return this.produtos;
   }
 
-  delete(produto: Produto){
-    
+  delete(produto: Produto) {
+    console.log(`[${this.title}#delete] produto:`, produto);
   }
 }
