7f35c100c619a4546af0ebb9643cae8f0adea303
dudushy <eduardotalarico1337@gmail.com>
2022-11-10 00:02:16 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarProdutosComponent } from './components/listar-produtos/listar-produtos.component';
+import { TestComponent } from './components/test/test.component';
 
 const routes: Routes = [
-  { path: '', component: ListarProdutosComponent }
+  { path: '', component: ListarProdutosComponent },
+  { path: 'test', component: TestComponent }
 ];
 
 @NgModule({
